6bb16789301215885a1595afdff448f17a6fa52e
Jany.Q <49868217+Qinjay@users.noreply.github.com>
2020-10-22 14:10:40 +0800

    Add files via upload

---
+++ common/config/config.dev.js
@@ -0,0 +1,19 @@
+ const appConfigDev={
+	// 开发
+	// addomain: 'https://devad.yunyutian.cn',
+	// outUrl: 'http://devs.yunyutian.cn',
+	// domain: 'https://devapi.yunyutian.cn',
+	
+	// 测试
+	addomain:'https://tad.yunyutian.cn',
+	outUrl:'http://stest.yunyutian.cn',
+	domain: 'https://tapi.yunyutian.cn',
+	// domain: 'http://localhost:12134',
+	
+	// // 正式
+	// addomain: 'https://ad.cwyyt.cn',
+	// outUrl: 'https://s.cwyyt.cn',
+	// domain: 'https://api.cwyyt.cn',
+}
+
+export default appConfigDev; 
---
+++ common/config/config.prod.js
@@ -0,0 +1,13 @@
+const appConfigProd = {
+	// 测试
+	addomain: 'https://tad.yunyutian.cn',
+	outUrl: 'http://stest.yunyutian.cn',
+	domain: 'https://tapi.yunyutian.cn'
+
+	// 正式
+	// addomain: 'https://ad.cwyyt.cn',
+	// outUrl: 'https://s.cwyyt.cn',
+	// domain: 'https://api.cwyyt.cn',
+}
+
+export default appConfigProd
